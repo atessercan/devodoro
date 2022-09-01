eb91eeb5485556d7266c03c095bf099dd8fea960
Sercan Ateş <sercanates91@gmail.com>
2022-09-01 14:00:03 +0300

    Added fonts and EsLint rules

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/global.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="Application logo" width="100px" />
+        <h1>DEVODORO</h1>
+        <h2>Menu</h2>
+      </header>
+      <p>
+        This is a Text!
+      </p>
+    </div>
+  );
+}
+
+export default App;
