06590c92fcb251b9a318444245029bd545cd2101
Sercan Ateş <sercanates91@gmail.com>
2022-09-07 17:41:17 +0300

    Added context provider for auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import Header from './components/organisms/Header';
 import Pomodoro from './components/organisms/Pomodoro';
 import './styles/global.scss';
 import 'animate.css';
+import AuthContext from './context/auth-context';
+import app from './helpers/firebase';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    app.auth.onAuthStateChanged(setCurrentUser);
+  }, []);
 
   useEffect(() => {
     setTimeout(() => {
@@ -13,15 +20,17 @@ function App() {
     }, 500);
   });
   return (
-    <div className="app">
-      <div className={isLoading ? 'hidden' : 'visible'}>
-        <Header />
-        <Pomodoro />
+    <AuthContext.Provider value={(currentUser, setCurrentUser)}>
+      <div className="app">
+        <div className={isLoading ? 'hidden' : 'visible'}>
+          <Header />
+          <Pomodoro />
+        </div>
+        <div className={isLoading ? 'visible' : 'hidden'}>
+          <div className="animate__animated animate__backOutUp">DEVODORO</div>
+        </div>
       </div>
-      <div className={isLoading ? 'visible' : 'hidden'}>
-        <div className="animate__animated animate__backOutUp">DEVODORO</div>
-      </div>
-    </div>
+    </AuthContext.Provider>
   );
 }
 
