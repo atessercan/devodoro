ecfc4b7112204a4c45359059cea0af1d11a0019b
Sercan Ateş <sercanates91@gmail.com>
2022-09-11 15:20:25 +0300

    Improved responsivity

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ function App() {
     <ThemeContext.Provider value={themeValues}>
       <AuthContext.Provider value={authValues}>
         <div className={theme === 'night' ? 'app-dark' : 'app-light'}>
-          <div className={isLoading ? 'hidden' : 'visible'}>
+          <div className={isLoading ? ' hidden' : 'main visible'}>
             <Header />
             <Pomodoro />
           </div>
